d9a4906c46273fa3eef7edea3145431dfb50c5d0
isaac stott <isaac.cloyd@gmail.com>
2020-06-05 10:24:45 -0600

    got full CRUD working on projects and actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Actions = require("../data/helpers/actionModel");
+const Projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
@@ -17,5 +18,70 @@ router.get('/', (req, res) => {
         });
       });
 });
+
+router.post('/:id/actions', (req, res) => {
+    // do your magic!
+    const body = req.body;
+    body.project_id = req.params.id
+  
+    Actions.insert(body)
+      .then(action => {
+        res.status(201).json(action)
+      })
+      .catch(err => {
+        console.log(err),
+        res.status(500).json({
+          error: "There was an error while saving the action to the database."
+        });
+      });
+});
+
+// function validatePost(req, res, next) {
+//     // do your magic!
+//     const post = req.body
+    
+//     if(post.text) {
+//       next()
+//     } else {
+//       res.status(400).json({message: "missing post data"});
+//     }
+// }
+
+router.delete("/:id", (req, res) => {
+    Actions.remove(req.params.id)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: "The action has been nuked" });
+        } else {
+          res.status(404).json({ message: "The action could not be found" });
+        }
+      })
+      .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+          message: "Error removing the action",
+        });
+      });
+});
+
+router.put("/:id", (req, res) => {
+    const changes = req.body;
+    Actions.update(req.params.id, changes)
+      .then(action => {
+        if (action) {
+          res.status(200).json(action);
+        } else {
+          res.status(404).json({ message: "The action could not be found" });
+        }
+      })
+      .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+          message: "Error updating the action",
+        });
+      });
+  });
 
 module.exports = router;
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -18,4 +18,78 @@ router.get('/', (req, res) => {
       });
 });
 
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+  
+    .then(user => {
+      if(user){
+        res.status(201).json(user);
+      } else {
+        res.status(500).json({
+        error: "There was an error while saving to the database"
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the project to the database."
+      });
+    })
+  
+});
+
+
+// function validateUser(req, res, next) {
+// // do your magic!
+// const user = req
+
+// if(user.body) {
+//     if(user.body.name) {
+//     next();
+//     } else {
+//     res.status(400).json({message: "missing required name field"});
+//     }
+// } else {
+//     res.status(400).json({errorMessage: "missing user data."});
+// }
+// }
+
+router.delete("/:id", (req, res) => {
+    Projects.remove(req.params.id)
+        .then(count => {
+        if (count > 0) {
+            res.status(200).json({ message: "The project has been nuked" });
+        } else {
+            res.status(404).json({ message: "The project could not be found" });
+        }
+        })
+        .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            message: "Error removing the project",
+        });
+        });
+});
+
+router.put("/:id", (req, res) => {
+    const changes = req.body;
+
+    Projects.update(req.params.id, changes)
+        .then(project => {
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: "The project could not be found" });
+        }
+        })
+        .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            message: "Error updating the project",
+        });
+        });
+});
+
 module.exports = router;
